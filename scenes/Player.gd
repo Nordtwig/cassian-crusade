9886b43d2c1ebade2dd7512de70b051728f5b624
Noah <nordqvist.gaming@gmail.com>
2023-12-06 20:20:06 +0100

    Export speed stat

--- scenes/Player.gd
+++ scenes/Player.gd
@@ -1,7 +1,9 @@
 extends Node2D
 
+@export var lateralSpeed : float = 20.5
+
 func _process(delta):
 	if Input.is_action_pressed("Left"):
-		position.x -= 1
+		position.x -= lateralSpeed * delta
 	if Input.is_action_pressed("Right"):
-		position.x += 1
+		position.x += lateralSpeed * delta
